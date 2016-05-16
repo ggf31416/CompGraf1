a8603c55a49ceb540b57126ec2546cfc799899f6
Guillermo Gabrielli <ggf31416@gmail.com>
2016-05-16 11:58:46 -0300

    altura

--- PruebaFisica/src/DeteccionColision.cpp
+++ PruebaFisica/src/DeteccionColision.cpp
@@ -109,3 +109,5 @@ void DeteccionColision::test(){
 
 
 
+
+
---
+++ PruebaFisica/src/PruebaFisica2.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : PruebaFisica2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "DeteccionColision.h"
+
+using namespace std;
+
+int main() {
+	DeteccionColision d;
+	d.test();
+	return 0;
+}
